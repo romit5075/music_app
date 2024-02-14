993e202aafd6a6e1b6b29ac09c085493d5c50831
romit5075 <romitdobariya5075@gmail.com>
2024-02-14 10:44:49 +0530

    add music library name called Howler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsPageModule } from './tabs/tabs.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, TabsPageModule],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, TabsPageModule,FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, }],
   bootstrap: [AppComponent],
 })
--- src/app/common/music/music.page.ts
+++ src/app/common/music/music.page.ts
@@ -2,76 +2,123 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MenuController, NavController } from '@ionic/angular';
 import { appData } from '../../API/homeMusic';
+import { Howl, Howler } from 'howler';
 
 @Component({
   selector: 'app-music',
   templateUrl: './music.page.html',
   styleUrls: ['./music.page.scss'],
 })
+
 export class MusicPage implements OnInit {
 
-  public progress = 0;
-  public isPlaying = false; // Track if music is currently playing
   public currentIndex = 0; // Index of the current music item
   public singleMusic: any;
+  public progress: number = 0;
+  public progressInterval: any;
+  public isPlaying = false;
+  activeTrack: any = null;
+  player: Howl | null = null;
 
-  
-  // Play music
-  play() {
-    console.log('hell')
-    this.isPlaying = !this.isPlaying; // Toggle isPlaying
+
+  currentTime: number = 0;
+  totalDuration: number = 0;
+
+  openMenu() {
+    throw new Error('Method not implemented.');
+  }
+
+  togglePlayPause(track: any) {
+    this.activeTrack = track;
+    if (!this.player) {
+      // Create the player if it doesn't exist
+      this.player = new Howl({
+        src: [track],
+        onplay: () => {
+          this.isPlaying = true;
+          this.startProgressTimer();
+        },
+        onpause: () => {
+          this.isPlaying = false;
+          this.stopProgressTimer();
+        },
+        onstop: () => {
+          this.isPlaying = false;
+          this.stopProgressTimer();
+        }
+      });
+    }
+
     if (this.isPlaying) {
-      // Logic to start playing music
-      console.log('Playing music...')
+      this.player.pause(); // Pause if currently playing
     } else {
-      // Logic to pause music
-    }
-  }
-  // Pause music
-  pause() {
-    console.log('pose')
-    this.isPlaying = !this.isPlaying; // Toggle isPlaying
-    if (!this.isPlaying) {
-      console.log('Pose music...')
-
+      this.player.play(); // Start playing if paused or stopped
     }
   }
 
-  togglePlayPause() {
-    this.isPlaying = !this.isPlaying; // Toggle isPlaying
-    if (this.isPlaying) {
-      this.play(); // Call play method if music is playing
-    } else {
-      this.pause(); // Call pause method if music is paused
-    }
+  startProgressTimer() {
+    this.progressInterval = setInterval(() => {
+      if (this.player && this.player.playing()) {
+        const currentTimeInSeconds = this.player.seek();
+        const totalDurationInSeconds = this.player.duration();
+        this.progress = (currentTimeInSeconds || 0) / totalDurationInSeconds * 1;
+
+        // Update current time and total duration
+        this.currentTime = currentTimeInSeconds;
+        this.totalDuration = totalDurationInSeconds;
+      }
+    }, 100);
   }
 
-  // Skip to next music
-  skipNext() {
-    this.currentIndex = (this.currentIndex + 1) % this.singleMusic.music.length;
-    // Logic to play next music
+
+  stopProgressTimer() {
+    clearInterval(this.progressInterval);
   }
 
-  // Skip to previous music
-  skipPrevious() {
-    this.currentIndex = (this.currentIndex - 1 + this.singleMusic.music.length) % this.singleMusic.music.length;
-    // Logic to play previous music
+  formatTime(timeInSeconds: number): string {
+    const minutes: number = Math.floor(timeInSeconds / 60);
+    const seconds: number = Math.floor(timeInSeconds % 60);
+    return `${this.padZero(minutes)}:${this.padZero(seconds)}`;
+  }
+
+  padZero(num: number): string {
+    return (num < 10 ? '0' : '') + num;
   }
 
   // Close page
   closePage() {
     this.navCtrl.back();
   }
+  skipNext() {
+    this.currentIndex = (this.currentIndex + 1) % this.singleMusic.length;
+    const nextTrackUrl = this.singleMusic.music[this.currentIndex].url;
+    this.togglePlayPause(nextTrackUrl);
+  }
 
-  // Open menu
-  openMenu() {
-    this.menuCtrl.open();
+  skipPrevious() {
+    this.currentIndex = (this.currentIndex - 1 + this.singleMusic.length) % this.singleMusic.music.length;
+    const previousTrackUrl = this.singleMusic.music[this.currentIndex].url;
+    this.togglePlayPause(previousTrackUrl);
+  }
+
+  // Function to get the duration of the music
+  getDuration(): number {
+    return this.player ? this.player.duration() : 0;
+  }
+
+  // Function to seek to a specific time in the music
+  seekTo(event: any) {
+    const newValue = event.detail.value as number;
+    const duration = this.player ? this.player.duration() : 0;
+    const seekTime = duration * (newValue / 1000); // Convert progress value to seconds
+    if (this.player) {
+      this.player.seek(seekTime); // Seek to the specified time in the song
+      this.progress = newValue; // Update the progress variable
+    }
   }
 
 
-
   constructor(private navCtrl: NavController, private menuCtrl: MenuController, private route: ActivatedRoute) { }
-
 
   ngOnInit() {
     const id = this.route.snapshot.params['id'];
@@ -87,6 +134,4 @@ export class MusicPage implements OnInit {
       this.currentIndex = parsedMusicData.findIndex((item: any) => item.id === id);
     });
   }
-
-
 }
